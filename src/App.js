a1fdfa8e7ebb59633fd7a08b22a4a73b1e3074cf
GH <gwanghyunjeon@gmail.com>
2022-08-22 17:38:27 +0900

    주야간 모드 전환 기능 삽입

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { useState } from 'react';
 
+const PushButton = styled.button`
+  font-size: 20px;
+  font-weight: 700;
+  height: 50px;
+  width: 200px;
+  border-radius: 20px;
+  background-color: ${(props) => (props.isNight ? 'white' : 'black')};
+  color: ${(props) => (props.isNight ? 'black' : 'white')};
+`;
 function App() {
+  const [isNight, setIsNight] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={isNight ? 'App-header' : 'App-header2'}>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>{`GH's space`}</p>
+        <PushButton
+          onClick={() => {
+            setIsNight(!isNight);
+          }}
+          isNight={isNight}
         >
-          Learn React : good
-        </a>
+          {isNight ? '주간모드로 바꾸기' : '야간모드로 바꾸기'}
+        </PushButton>
       </header>
     </div>
   );
